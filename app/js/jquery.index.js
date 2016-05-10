67a6589edfe9f0834a365ca780c769c42b800810
iversontb <petruninevgeniy@gmial.com>
2016-05-10 18:52:39 +0300

    index

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -1,3 +1,43 @@
-( function(){
+$( function(){
 
-} )();
+    "use strict";
+
+    $.each( $( '.promo' ), function() {
+        new Sliders ( $( this ) );
+    } );
+
+} );
+
+var Sliders = function( obj ) {
+
+    //private properties
+    var _obj = obj,
+        _promoSlider = _obj.find( '.promo__swiper'),
+        _promoNextSlider = _obj.find( '.promo-button-next'),
+        _promoPrevSlider = _obj.find( '.promo-button-prev'),
+        _slider;
+
+
+    //private methods
+    var _initSlider = function() {
+
+            _slider = new Swiper ( _promoSlider, {
+                autoplay: 4000,
+                speed: 500,
+                effect : 'fade',
+                loop: true,
+                nextButton: _promoNextSlider,
+                prevButton: _promoPrevSlider
+            } );
+
+        },
+        _init = function() {
+            _initSlider();
+        };
+
+    //public properties
+
+    //public methods
+
+    _init();
+};
